487efbfb8da9a9130ee1b7f7043e6a7cb3543a33
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-18 09:38:51 +0300

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 from database.filters_mdb import(
    add_filter,
