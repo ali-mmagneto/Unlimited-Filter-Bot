b5dc899e6c6888b4d2d9eb9baa4a8e6d38f21d00
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-18 09:37:52 +0300

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 from script import Script
 from database.filters_mdb import del_all, find_filter
