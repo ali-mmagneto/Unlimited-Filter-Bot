3583d0d2f2728de9cd96ca5f1228c1ec7413b51d
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-18 10:06:16 +0300

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -30,7 +30,7 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Command Help", callback_data="help_data")
+                    InlineKeyboardButton("Komut Yardımı", callback_data="help_data")
                 ]
             ]
         )
@@ -47,12 +47,10 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("Hakkımda", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Destek", url="https://t.me/mmagneto")
                 ]
             ]
         )
@@ -70,11 +68,11 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "Bot Owner", url="https://t.me/mmagneto")
                 ],
                 [
-                    InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
+                    InlineKeyboardButton("Geri", callback_data="help_data"),
+                    InlineKeyboardButton("Kapat", callback_data="close_data"),
                 ]                
             ]
         )
