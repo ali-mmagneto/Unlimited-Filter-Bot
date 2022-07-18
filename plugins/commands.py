8cf568835a2bfc2918806640d1375299b62e38ca
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-18 09:38:12 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 from script import Script
 from plugins.helpers import humanbytes
